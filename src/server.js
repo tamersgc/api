a20fe4d79d8891c8e6668236172e6c87d779a12f
Tamer <tamersgc@gmail.com>
2023-05-30 14:44:57 -0300

    Query and Params

--- src/server.js
+++ src/server.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const app = express()
 
-app.get("/", ()=>{
-    
+app.get("/message/:id/:user", (request, response)=>{
+    const {id,user} = request.params;
+    response.send(`Id = ${id} USER = ${user} `)
+    console.log("eitaaa")
+});
+
+
+app.get("/users", (request, response)=>{
+    const {page,limit} = request.query;
+    response.send(`Page = ${page}  Limit ${limit}`)
     console.log("eitaaa")
 });
 
 
 
-app.listen(3333)
+app.listen(3333, ()=>{console.log("Server Running...")})
